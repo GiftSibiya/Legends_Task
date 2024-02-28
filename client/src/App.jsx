69496894b9ba57e9183a8515d1fa772206c391be
GiftSibiya <giftsibiya.gb@gmail.com>
2024-02-28 18:59:17 +0200

    time's up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // Corrected import path for Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Corrected import path for Bootstrap JS
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TaskTable from "./Table/TaskTable";
 
 import { Toaster } from "react-hot-toast";
+import Login from "./screens/Login";
 
 export default function App() {
   return (
     <>
       <Toaster></Toaster>
-      <TaskTable />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<TaskTable />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
