6997683c9f3d25d32a6ba485529d46903c873107
GiftSibiya <giftsibiya.gb@gmail.com>
2024-02-29 19:52:47 +0200

    Task and user Routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,11 +1,23 @@
+/// IMPORT DEPENDENCIES ///
 import express from "express";
-import { create, get, Updated, Delete } from "../controllers/Controller.js";
+import {
+  create,
+  get,
+  Updated,
+  Delete,
+  getUser,
+} from "../controllers/Controller.js";
+/// -- ///
 
 const routers = express.Router();
 
+/// TASK ROUTES
 routers.post("/create", create);
 routers.get("/get", get);
 routers.put("/update/:id", Updated);
 routers.delete("/delete/:id", Delete);
+
+/// USER ROUTES
+routers.get("/user", getUser);
 
 export default routers;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
+/// IMPORT DEPENDENCIES ///
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routers from "./routes/routes.js";
 import dbCon from "./utlis/db.js";
 
-dotenv.config();
-const app = express();
-dbCon();
-app.use(cors());
+///--///
 app.use(express.json());
+const app = express();
+dotenv.config();
+app.use(cors());
+dbCon();
+
+/// THIS WILL PREFIX THE ROUTES WITH /API. USE POSTMAN TO TEST
 app.use("/api", routers);
 
 app.listen(process.env.PORT, () => {
