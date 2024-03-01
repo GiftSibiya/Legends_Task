e2f6d86b050aa25f40d7e891b8b1d9c0e7cce8bb
GiftSibiya <giftsibiya.gb@gmail.com>
2024-03-01 07:17:17 +0200

    Added connection string

--- backend/utlis/db.js
+++ backend/utlis/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const dbCon=async()=>{
-    try {
-        await mongoose.connect(process.env.DB_URL)
-        console.log('Mongodb is conntected')
-    } catch (error) {
-        console.log(error)
-    }
-}
+const dbCon = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sibiyabobo:b0b0@cluster0.xe42gpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("Mongodb is conntected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export default dbCon
+export default dbCon;
